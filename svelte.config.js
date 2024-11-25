efda00cbc63436b751437fa6e9a92bf4c0c1b0aa
c8e4 <c8e4@proton.me>
2024-11-25 23:56:57 +0100

    prerender deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			strict: false
-		}),
+		adapter: adapter({}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/exle-svelte' : ''
 		}
