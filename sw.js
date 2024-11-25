9a9ce2fc1416c194b1b8b9aade8f8c266fdf9122
c8e4 <c8e4@proton.me>
2024-11-25 23:29:50 +0100

    Updates

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', () => {
+	// Dummy install event
+});
+
+self.addEventListener('activate', () => {
+	// Dummy activate event
+});
+
+self.addEventListener('fetch', () => {
+	// Dummy fetch event
+});
