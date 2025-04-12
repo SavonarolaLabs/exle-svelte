0b0f495c5afe0715d888a9238effcadf716bd02e
c8e4 <c8e4@proton.me>
2025-04-12 16:49:22 +0200

    empty transactions page

---
+++ src/components/loan/EmptyTransactions.svelte
@@ -0,0 +1,10 @@
+<script>
+	import EmptyBox from '../../icons/EmptyBox.svelte';
+</script>
+
+<div class="flex grow flex-col items-center justify-center py-16">
+	<div class="mb-6">
+		<EmptyBox />
+	</div>
+	<div class="mb-1 text-center">You haven’t made any transactions on EXLE yet.</div>
+</div>
--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+	import EmptyTransactions from '../../components/loan/EmptyTransactions.svelte';
 	import TransactionTable from '../../components/txhistory/TransactionTable.svelte';
 	import { transactions } from '../../data/TxHistory';
 </script>
 
-<div class="md:px-4 xl:px-0">
+<div class="flex grow flex-col md:px-4 xl:px-0">
 	<div class="mb-4 mt-10">
 		<div class="mb-1 text-xl font-semibold">Transaction History</div>
 		<div class="opacity-[0.6]">
 			{transactions.length} Transaction{transactions.length != 1 ? 's' : ''}
 		</div>
 	</div>
-	<TransactionTable></TransactionTable>
+	{#if transactions?.length > 0}
+		<TransactionTable></TransactionTable>
+	{:else}
+		<EmptyTransactions></EmptyTransactions>
+	{/if}
 </div>
