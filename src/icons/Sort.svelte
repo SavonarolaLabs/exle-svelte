eee53bd8a2aab8483bd4cd203fabd7cbe0c88912
c8e4 <c8e4@proton.me>
2025-04-13 00:50:48 +0200

    current color sort icon

--- src/icons/Sort.svelte
+++ src/icons/Sort.svelte
@@ -10,7 +10,12 @@
 	xmlns="http://www.w3.org/2000/svg"
 	class="{direction === 'asc' ? 'rotate-180' : ''} transition-transform"
 >
-	<path d="M2.5 5.8335H17.5" stroke="#0C0D2D" stroke-width="1.2" stroke-linecap="round" />
-	<path d="M5 10H15" stroke="#0C0D2D" stroke-width="1.2" stroke-linecap="round" />
-	<path d="M8.3335 14.1665H11.6668" stroke="#0C0D2D" stroke-width="1.2" stroke-linecap="round" />
+	<path d="M2.5 5.8335H17.5" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" />
+	<path d="M5 10H15" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" />
+	<path
+		d="M8.3335 14.1665H11.6668"
+		stroke="currentColor"
+		stroke-width="1.2"
+		stroke-linecap="round"
+	/>
 </svg>
