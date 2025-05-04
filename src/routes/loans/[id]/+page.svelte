c3c2bb13e89b454c66d50d75d3b67915d05b1a4a
c8e4 <c8e4@proton.me>
2025-05-04 17:59:55 +0200

    fix async load of a loan by id

--- src/routes/loans/[id]/+page.svelte
+++ src/routes/loans/[id]/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 	import LoanDetails from '../../../components/loan/LoanDetails.svelte';
 	import { repayments } from '../../../stores/ui';
 
@@ -8,14 +7,12 @@
 	let loanId;
 
 	$: loanId = $page.params.id;
-
-	onMount(() => {
-		loan = $repayments.find((l) => l.loanId == loanId);
-	});
+	$: loan = $repayments.find((l) => l.loanId === loanId);
 </script>
 
 {#if loan}
-	<LoanDetails {loan}></LoanDetails>
-{:else}
+	<LoanDetails {loan} />
+{:else if $repayments.length > 0}
+	{$repayments.length}
 	Loan not found.
 {/if}
