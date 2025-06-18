7ac9654ff0ac3f3d86cb3e82de4aea32c51f8d96
c8e4 <c8e4@proton.me>
2025-06-18 21:10:32 +0200

    Updates

---
+++ _app/immutable/chunks/DCUBAi0u.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_eltgk8)==null?void 0:s.base)??"/exle-svelte";var e;const l=((e=globalThis.__sveltekit_eltgk8)==null?void 0:e.assets)??t;export{l as a,t as b};
